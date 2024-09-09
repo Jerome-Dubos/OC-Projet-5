6a927aa96a809f82e9dce23fdd98c0b952a25598
Jerome Dubos <jeromedubos67@gmail.com>
2024-09-09 18:48:06 +0200

    Finalisation carrousel

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,65 @@
+let bannerImage = document.querySelector(".banner-img")
+let dotSelected = document.querySelector('.dot_selected')
+let arrowLeft = document.querySelector('.arrow_left')
+let arrowRight = document.querySelector('.arrow_right')
+let imageActive = 0
 const slides = [
 	{
-		"image": "slide1.jpg",
+		"image": "./assets/images/slideshow/slide1.jpg",
 		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image": "slide2.jpg",
+		"image": "./assets/images/slideshow/slide2.jpg",
 		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image": "slide3.jpg",
+		"image": "./assets/images/slideshow/slide3.jpg",
 		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image": "slide4.png",
+		"image": "./assets/images/slideshow/slide4.png",
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-let arrowLeft = document.querySelector('.arrow_left')
-arrowLeft.addEventListener('click', () => {
-	console.log("Bravo ca marche a gauche")
-})
-
-let arrowRight = document.querySelector('.arrow_right')
-arrowRight.addEventListener('click', () => {
-	console.log('Bravo ca marche a droite aussi')
-})
-
-let dots = `<div class="dot"></div>`
-
-for (let i = 0; i < slides.length; i++) {
-	let dotsSlider = document.querySelector(".dots")
-	dotsSlider.innerHTML += dots
-	let divDot = document.querySelector(".dots .dot")
-	divDot.classList.add("dot_selected")
+function addDots() {
+	let dots = `<span class="dot"></span>`
+	for (let i = 0; i < slides.length; i++) {
+		let dotsSlider = document.querySelector(".dots")
+		dotsSlider.innerHTML += dots
+		let divDot = document.querySelector(".dots .dot")
+		divDot.classList.add("dot_selected")
+	}
+	document.querySelectorAll(".dot").forEach((dot, i) => {
+		dot.addEventListener('click', () => {
+			imageActive = i
+			bannerImage.src = slides[imageActive].image
+			addSelectedDot()
+		})
+	})
 }
+
+arrowLeft.addEventListener('click', () => {
+	imageActive--
+	if (imageActive < 0) {
+		imageActive = slides.length - 1
+	}
+	bannerImage.src = slides[imageActive].image
+	addSelectedDot()
+})
+
+arrowRight.addEventListener('click', () => {
+	imageActive++
+	if (imageActive >= slides.length) {
+		imageActive = 0
+	}
+	bannerImage.src = slides[imageActive].image
+	addSelectedDot()
+})
+
+function addSelectedDot() {
+	document.querySelector('.dot_selected').classList.remove('dot_selected')
+	document.querySelectorAll(".dot")[imageActive].classList.add("dot_selected")
+}
+
+addDots()
